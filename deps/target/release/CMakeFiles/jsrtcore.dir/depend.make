abe78a81eb22723ffa21fec72b939d4cd97ec574
LEI Zongmin <leizongmin@gmail.com>
2025-09-04 22:50:08 +0800

    chore: 清理构建过程中生成的临时文件和CMake缓存文件

--- deps/target/release/CMakeFiles/jsrtcore.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for jsrtcore.
-# This may be replaced when dependencies are built.
--- deps/target/release/deps/libuv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lei/work/jsrt/deps/libuv
-# Build directory: /home/lei/work/jsrt/deps/target/release/deps/libuv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
